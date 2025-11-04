d8788a069ed8cc2e3c36e0e85219beb3bcc83809
CHALLA  HARSHAVARDHAN <25kq1a4726@pace.ac.in>
2025-11-04 14:38:33 +0530

    Add files via upload

--- HARSHA.C
+++ HARSHA.C
@@ -1,16 +1,15 @@
 #include<stdio.h>
 #include<conio.h>
-void main ()
+void main()
 {
- int a,b,sum,diff;
- clrscr();
- printf("enter the frist number");
- scanf("%d",&a);
- printf("enter the second number");
- scanf("%d",&b);
- sum=a+b;
- diff=a-b;
- printf("\n addition of %d amd %d is :%d",a,b,sum);
- printf("\n subtraction of %d and %d is :%d",a,b,diff);
- getch ();
+ int a=5;
+ int b=++a;
+ int c=a++;
+ int d=a;
+ printf("%d,%d,%d",a,b,c,d);
+ getch();
  }
+
+
+
+
